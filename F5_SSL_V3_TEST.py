b6651e65372e6fe0a309bd289ec2cb092f951586
Olichous <eche.ece@gmail.com>
2014-10-28 19:24:48 +0100

    Modification of function and the loop return the good value
    Use of Socket Module for DNS query and use of xlwt for editing an .xls
    
    Use of Socket Module for DNS query and use of xlwt for editing an .xls

--- F5_SSL_V3_TEST.py
+++ F5_SSL_V3_TEST.py
@@ -9,57 +9,74 @@
 # Function getting F5 Virtual Server.
 
 def get_VirtualServer_list(F5):
-	try:
-		return F5.LocalLB.VirtualServer.get_list()
-	except	Exception, e:
-		print e
+    try:
+        return F5.LocalLB.VirtualServer.get_list()
+    except    Exception, e:
+        print e
 
 # Function getting F5 Virtual Server HTTP Class -- Use F5 Object
 
-def get_all_VirtualServer_VirtualServerHttpClass(F5):
-	try:
-		VirtualServer = get_VirtualServer_list(F5)
-		return VirtualServer, F5.LocalLB.VirtualServer.VirtualServerHttpClass.get_profile_name(VirtualServer)
-	except	Exception, e:
-		print e
+def get_all_VirtualServer_ProfileType(F5):
+    try:
+        VirtualServer = get_VirtualServer_list(F5)
+        return VirtualServer, F5.LocalLB.VirtualServer.get_profile(VirtualServer)
+    except    Exception, e:
+        print e
 
 # Function getting F5 VIP -- Use F5 object and VirtualServer 
 
 def get_VirtualServer_destination(F5, VS):
-	try:
-		return F5.LocalLB.VirtualServer.get_destination([VS])
-	except	Exception, e:
-		print e
+    try:
+        return F5.LocalLB.VirtualServer.get_destination([VS])
+    except    Exception, e:
+        print e
 
 # Main 
 
 if __name__ == "__main__":
 
-''' Import Bigsuds Module / getpass '''
-import bigsuds
-import getpass
+    ''' Import Module : Bigsuds (F5 Module) / Getpass (Password) / Socket (DNS) / xlwt (Writing Excel)'''
+    import bigsuds
+    import getpass
+    import socket
+    from xlwt import Workbook 
 
-# List of equipement -- Uncomment and change your IP here
+    # List of Equipement -- Put your F5-IP in F5_IP_LIST.txt
+    F5_BIGIP_IP_LIST = [line.strip() for line in open('F5_IP_LIST.txt', 'r')]
+    print F5_BIGIP_IP_LIST
+        
+    # Obtain Login of equipement
+    print "Give your Login of your(s) Equipement(s)",
+    login = raw_input()
+    
+    # Obtain password of equipement
+    print "Give Your Password of your(s) Equipement(s)",
+    pswd = getpass.getpass() 
+    
+    # Create an .xls and a sheet and create 
+    book = Workbook()
+    feuill1 = book.add_sheet('VIP_SSL')
 
-# F5_BIGIP_IP_LIST = ['IP']
+    # For every equipement we have to connect to the equipement,
+    # obtain all the VIP and find all the VIP with a PROFILE_TYPE_CLIENT_SSL activate
+    # We put all the VIP SSL in a xls
 
-# Obtain Login of equipement
-print "Give your Login of your(s) Equipement(s)",
-login = raw_input()
-
-# Obtain password of equipement
-print "Give Your Password of your(s) Equipement(s)",
-pswd = getpass.getpass() 
-
-# For every equipement we have to connect to the equipement,
-# obtain all the VIP SSL
-
-for F5_BIGIP_IP in F5_BIGIP_IP_LIST:
-	F5_BIGIP = bigsuds.BIGIP(F5_BIGIP_IP, username = login, password = pswd)
-	if  get_VirtualServer_VirtualServerHttpClass(F5_BIGIP) == 'SSL':
-	 	print get_VirtualServer_destination(F5_BIGIP)
-	else:
-
-
-
-
+    j = 0
+    for F5_BIGIP_IP in F5_BIGIP_IP_LIST:
+        print " Pour le LB %s = %s \n" %(socket.gethostbyaddr(F5_BIGIP_IP)[0], F5_BIGIP_IP)
+        pswd = "NTV{}&adm".format(j+1)
+        print pswd
+        F5_BIGIP = bigsuds.BIGIP(F5_BIGIP_IP, username = login, password = pswd)
+        VS, VS_PROFILE_ATTRIBUTE = get_all_VirtualServer_ProfileType(F5_BIGIP)
+        VS_IP = get_VirtualServer_destination(F5_BIGIP, VS)
+        feuill1.write(0,j,socket.gethostbyaddr(F5_BIGIP_IP)[0])
+        i=0
+        for vip in VS_PROFILE_ATTRIBUTE:
+            for prof in vip:
+                if  prof['profile_type'] == 'PROFILE_TYPE_CLIENT_SSL':
+                    print "Cette IP : %s est a checker \n" % VS_IP[i]['address']
+                    feuill1.write(i+1,j,VS_IP[i]['address'])    
+            i+=1
+        j+=1
+ 
+    book.save('test.xls')
